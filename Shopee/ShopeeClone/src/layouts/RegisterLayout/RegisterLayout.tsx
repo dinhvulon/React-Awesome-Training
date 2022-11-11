db35df66948f7488b52774228fc7da7e3e591606
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-11-11 15:41:14 +0700

    Code UI cho RegisterLayout

--- Shopee/ShopeeClone/src/layouts/RegisterLayout/RegisterLayout.tsx
+++ Shopee/ShopeeClone/src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -1,11 +1,15 @@
+import Footer from 'src/components/Footer'
+import RegisterHeader from 'src/components/RegisterHeader'
+
 interface Props {
   children?: React.ReactNode
 }
 export default function RegisterLayout({ children }: Props) {
   return (
     <div>
-      RegisterLayout
+      <RegisterHeader />
       {children}
+      <Footer />
     </div>
   )
 }
