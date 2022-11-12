c5b3cd374075a297db1686a764e71839f50b8e7f
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-11-12 20:15:33 +0700

    Khai báo interface cho auth và thực hiện gọi api register

--- Shopee/ShopeeClone/src/pages/Register/Register.tsx
+++ Shopee/ShopeeClone/src/pages/Register/Register.tsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from '@tanstack/react-query'
+import { omit } from 'lodash'
 import { schema, Schema } from 'src/utils/rules'
 import Input from 'src/components/Input'
+import { registerAccount } from 'src/apis/auth.api'
 
 type FormData = Schema
 
@@ -11,21 +14,21 @@ export default function Register() {
     register,
     handleSubmit,
     watch,
-    getValues,
     formState: { errors }
   } = useForm<FormData>({
     resolver: yupResolver(schema)
   })
-  const onSubmit = handleSubmit(
-    (data) => {
-      console.log(data)
-    },
-    (data) => {
-      const password = getValues('password')
-
-      console.log(password)
-    }
-  )
+  const registerAccountMutation = useMutation({
+    mutationFn: (body: Omit<FormData, 'confirm_password'>) => registerAccount(body)
+  })
+  const onSubmit = handleSubmit((data) => {
+    const body = omit(data, ['confirm_password'])
+    registerAccountMutation.mutate(body, {
+      onSuccess: (data) => {
+        console.log(data)
+      }
+    })
+  })
 
   const value = watch()
   console.log(value)
