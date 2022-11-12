5805938c522d9d7cf5d100bd98550b9f80bd7faf
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-11-12 11:11:11 +0700

    Custom class .container Tailwindcss bằng cách tạo plugin

--- Shopee/ShopeeClone/src/components/Footer/Footer.tsx
+++ Shopee/ShopeeClone/src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className='py-16 bg-neutral-100'>
-      <div className='max-w-7xl mx-auto px-4'>
+      <div className='container'>
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
           <div className='lg:col-span-1'>
             <div>© 2022 Shopee. Tất cả các quyền được bảo lưu.</div>
