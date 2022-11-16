bdf231c164cf90e74b4849b8284cd63df8199ceb
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-11-16 12:09:03 +0700

    Setup Protected Route và Rejected Route trong React Router

--- Shopee/ShopeeClone/src/components/Header/Header.tsx
+++ Shopee/ShopeeClone/src/components/Header/Header.tsx
@@ -48,7 +48,7 @@ export default function Header() {
             renderPopover={
               <div className='bg-white relative shadow-md rounded-sm border border-gray-200'>
                 <Link
-                  to='/'
+                  to='/profile'
                   className='block py-3 px-4 hover:bg-slate-100 bg-white hover:text-cyan-500 w-full text-left'
                 >
                   Tài khoản của tôi
