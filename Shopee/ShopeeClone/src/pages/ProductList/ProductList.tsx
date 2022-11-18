b6752823e1f6e6595573ebd240043699a9ba89a4
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-11-18 17:27:50 +0700

    Hiển thị product và format number với Intl

--- Shopee/ShopeeClone/src/pages/ProductList/ProductList.tsx
+++ Shopee/ShopeeClone/src/pages/ProductList/ProductList.tsx
@@ -13,7 +13,7 @@ export default function ProductList() {
       return productApi.getProducts(queryParams)
     }
   })
-  console.log(queryParams)
+  console.log(data)
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
@@ -24,11 +24,10 @@ export default function ProductList() {
           <div className='col-span-9'>
             <SortProductList />
             <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
-              {Array(30)
-                .fill(0)
-                .map((_, index) => (
-                  <div className='col-span-1' key={index}>
-                    <Product />
+              {data &&
+                data.data.data.products.map((product) => (
+                  <div className='col-span-1' key={product._id}>
+                    <Product product={product} />
                   </div>
                 ))}
             </div>
