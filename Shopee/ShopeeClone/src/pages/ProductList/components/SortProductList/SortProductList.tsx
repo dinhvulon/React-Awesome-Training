2b1f416fc5b772243e7171ef93f2e8945191e783
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-11-22 21:25:37 +0700

    Đưa các component vào folder ProductList/components

--- Shopee/ShopeeClone/src/pages/ProductList/components/SortProductList/SortProductList.tsx
+++ Shopee/ShopeeClone/src/pages/ProductList/components/SortProductList/SortProductList.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import { sortBy, order as orderConstant } from 'src/constants/product'
-import { QueryConfig } from '../ProductList'
+import { QueryConfig } from '../../ProductList'
 import { ProductListConfig } from 'src/types/product.type'
 import { createSearchParams, Link, useNavigate } from 'react-router-dom'
 import path from 'src/constants/path'
