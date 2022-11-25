8fda46d09dcc6b9acb598c10c294f00c93975585
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-11-25 19:07:19 +0700

    Tách component QuantityController

--- Shopee/ShopeeClone/src/pages/ProductDetail/ProductDetail.tsx
+++ Shopee/ShopeeClone/src/pages/ProductDetail/ProductDetail.tsx
@@ -5,11 +5,13 @@ import { useParams } from 'react-router-dom'
 import productApi from 'src/apis/product.api'
 import InputNumber from 'src/components/InputNumber'
 import ProductRating from 'src/components/ProductRating'
+import QuantityController from 'src/components/QuantityController'
 import { Product as ProductType, ProductListConfig } from 'src/types/product.type'
 import { formatCurrency, formatNumberToSocialStyle, getIdFromNameId, rateSale } from 'src/utils/utils'
 import Product from '../ProductList/components/Product'
 
 export default function ProductDetail() {
+  const [buyCount, setBuyCount] = useState(1)
   const { nameId } = useParams()
   const id = getIdFromNameId(nameId as string)
   const { data: productDetailData } = useQuery({
@@ -34,7 +36,7 @@ export default function ProductDetail() {
     staleTime: 3 * 60 * 1000,
     enabled: Boolean(product)
   })
-  console.log(productsData)
+
   useEffect(() => {
     if (product && product.images.length > 0) {
       setActiveImage(product.images[0])
@@ -79,6 +81,10 @@ export default function ProductDetail() {
 
   const handleRemoveZoom = () => {
     imageRef.current?.removeAttribute('style')
+  }
+
+  const handleBuyCount = (value: number) => {
+    setBuyCount(value)
   }
 
   if (!product) return null
@@ -172,38 +178,13 @@ export default function ProductDetail() {
               </div>
               <div className='mt-8 flex items-center'>
                 <div className='capitalize text-gray-500'>Số lượng</div>
-                <div className='ml-10 flex items-center'>
-                  <button className='flex h-8 w-8 items-center justify-center rounded-l-sm border border-gray-300 text-gray-600'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      fill='none'
-                      viewBox='0 0 24 24'
-                      strokeWidth={1.5}
-                      stroke='currentColor'
-                      className='h-4 w-4'
-                    >
-                      <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 12h-15' />
-                    </svg>
-                  </button>
-                  <InputNumber
-                    value={1}
-                    className=''
-                    classNameError='hidden'
-                    classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
-                  />
-                  <button className='flex h-8 w-8 items-center justify-center rounded-r-sm border border-gray-300 text-gray-600'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      fill='none'
-                      viewBox='0 0 24 24'
-                      strokeWidth={1.5}
-                      stroke='currentColor'
-                      className='h-4 w-4'
-                    >
-                      <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
-                    </svg>
-                  </button>
-                </div>
+                <QuantityController
+                  onDecrease={handleBuyCount}
+                  onIncrease={handleBuyCount}
+                  onType={handleBuyCount}
+                  value={buyCount}
+                  max={product.quantity}
+                />
                 <div className='ml-6 text-sm text-gray-500'>{product.quantity} sản phẩm có sẵn</div>
               </div>
               <div className='mt-8 flex items-center'>
--- api-document.md
+++ api-document.md
@@ -174,16 +174,17 @@ Ví du: `products?page=1&limit=30`
 Method: GET
 
 Query Params:
-`page`: number. Số trang. Mặc định là 1
-`limit`: number. Số product trên 1 trang. Mặc định là 30
-`order`: 'desc' || 'asc'. Sắp xếp theo thứ tự. Mặc định là 'desc'
-`sort_by`: 'createdAt' || 'view' || 'sold' || 'price'. Sắp xếp theo trường. Mặc định là 'createdAt'.
-`category`: categoryId. Lọc sản phẩm theo category
-`exclude`: productId. Loại trừ sản phẩm nào đó
-`rating_filter`: number. Lọc sản phẩm có số sao lớn hơn hoặc bằng rating_filter
-`price_max`: number. Giá cao nhất
-`price_min`: number. Giá thấp nhất
-`name`: string. Tên sản phẩm (lưu ý Tên sản phẩm tiếng Việt phải gõ đầy đủ dấu)
+
+- `page`: number. Số trang. Mặc định là 1
+- `limit`: number. Số product trên 1 trang. Mặc định là 30
+- `order`: 'desc' || 'asc'. Sắp xếp theo thứ tự. Mặc định là 'desc'
+- `sort_by`: 'createdAt' || 'view' || 'sold' || 'price'. Sắp xếp theo trường. Mặc định là 'createdAt'.
+- `category`: categoryId. Lọc sản phẩm theo category
+- `exclude`: productId. Loại trừ sản phẩm nào đó
+- `rating_filter`: number. Lọc sản phẩm có số sao lớn hơn hoặc bằng rating_filter
+- `price_max`: number. Giá cao nhất
+- `price_min`: number. Giá thấp nhất
+- `name`: string. Tên sản phẩm (lưu ý Tên sản phẩm tiếng Việt phải gõ đầy đủ dấu)
 
 Response
 
@@ -249,13 +250,14 @@ Query Params:
 `status`: Trạng thái đơn hàng
 
 Thông tin `status`:
--1: Sản phẩm đang trong giỏ hàng
-0: Tất cả sản phâm
-1: Sản phẩm đang đợi xác nhận từ chủ shop
-2: Sản phẩm đang được lấy hàng
-3: Sản phẩm đang vận chuyển
-4: San phẩm đã được giao
-5: Sản phẩm đã bị hủy
+
+- -1: Sản phẩm đang trong giỏ hàng
+- 0: Tất cả sản phâm
+- 1: Sản phẩm đang đợi xác nhận từ chủ shop
+- 2: Sản phẩm đang được lấy hàng
+- 3: Sản phẩm đang vận chuyển
+- 4: San phẩm đã được giao
+- 5: Sản phẩm đã bị hủy
 
 ## Update purchase: `/purchases/update-purchase`
 
