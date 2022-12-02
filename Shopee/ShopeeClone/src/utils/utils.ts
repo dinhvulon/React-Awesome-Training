c7aec45bc85729b0f3d50bb260463bd668618e7d
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-12-02 15:55:38 +0700

    Thực hiện chức năng upload ảnh

--- Shopee/ShopeeClone/src/utils/utils.ts
+++ Shopee/ShopeeClone/src/utils/utils.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from 'axios'
+import config from 'src/constants/config'
 import HttpStatusCode from 'src/constants/httpStatusCode.enum'
-import { string } from 'yup'
+import userImage from 'src/assets/images/user.svg'
 
 export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
   // eslint-disable-next-line import/no-named-as-default-member
@@ -39,3 +40,5 @@ export const getIdFromNameId = (nameId: string) => {
   const arr = nameId.split('-i-')
   return arr[arr.length - 1]
 }
+
+export const getAvatarUrl = (avatarName?: string) => (avatarName ? `${config.baseUrl}images/${avatarName}` : userImage)
