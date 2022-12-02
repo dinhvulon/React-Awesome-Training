5d8dcbca0a438c68fdc8a0f29013269d91f46856
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-12-02 14:28:35 +0700

    Fix lỗi component DateSelect và thực hiện cập nhật profile

--- Shopee/ShopeeClone/src/components/NavHeader/NavHeader.tsx
+++ Shopee/ShopeeClone/src/components/NavHeader/NavHeader.tsx
@@ -6,6 +6,7 @@ import path from 'src/constants/path'
 import { purchasesStatus } from 'src/constants/purchase'
 import { AppContext } from 'src/contexts/app.context'
 import Popover from '../Popover'
+import userImage from 'src/assets/images/user.svg'
 
 export default function NavHeader() {
   const { setIsAuthenticated, isAuthenticated, setProfile, profile } = useContext(AppContext)
@@ -22,7 +23,7 @@ export default function NavHeader() {
   const handleLogout = () => {
     logoutMutation.mutate()
   }
-
+  console.log(profile)
   return (
     <div className='flex justify-end'>
       <Popover
@@ -86,11 +87,7 @@ export default function NavHeader() {
           }
         >
           <div className='mr-2 h-6 w-6 flex-shrink-0'>
-            <img
-              src='https://cf.shopee.vn/file/d04ea22afab6e6d250a370d7ccc2e675_tn'
-              alt='avatar'
-              className='h-full w-full rounded-full object-cover'
-            />
+            <img src={profile?.avatar || userImage} alt='avatar' className='h-full w-full rounded-full object-cover' />
           </div>
           <div>{profile?.email}</div>
         </Popover>
